eee52eec6da5ea16c3c15fb110d365f5d4e220e9
Vijay Singh Kanwal <vijaykanwal90@gmail.com>
2023-07-15 22:56:50 +0530

    inital commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div>
+      <div className='previous-output'></div>
+      <div className='current-output'></div>
+      </div>
+     <div> 
+    <button>AC</button>
+    <button>DEl</button>
+    <button>/</button>
+    <button>1</button>
+    <button>2</button>
+    <button>3</button>
+    <button>X</button>
+    <button>4</button>
+    <button>5</button>
+    <button>6</button>
+    <button>+</button>
+    <button>7</button>
+    <button>8</button>
+    <button>9</button>
+    <button>-</button>
+    <button>0</button>
+    <button>=</button>
+     </div>
+    </div>
+  );
+}
+
+export default App;
